1d130a25454a3582ec1095f04457670c1c6be923
Rouven Hurling <rouven@supersonic-group.com>
2025-09-26 17:12:30 +0200

    WIP

--- nodes/Qonekto/v20250926/v20250926.node.ts
+++ nodes/Qonekto/v20250926/v20250926.node.ts
@@ -1,16 +1,16 @@
 import { INodeType, INodeTypeDescription } from 'n8n-workflow';
 import { NODE_DESCRIPTION } from '../base/node';
-import * as doc from './openapi.json';
+import doc from './openapi.json';
 import { N8NPropertiesBuilder, N8NPropertiesBuilderConfig } from '@devlikeapro/n8n-openapi-node';
 import QonektoOperationParser from '../openapi/QonektoOperationsParser';
 import OpenApiDocConverter from '../openapi/OpenApiDocConverter';
-import { OpenAPIV3 } from 'openapi-types';
 
 const config: N8NPropertiesBuilderConfig = {
 	operation: new QonektoOperationParser(),
 };
-const parser = new N8NPropertiesBuilder(OpenApiDocConverter(doc as OpenAPIV3.Document), config);
+const parser = new N8NPropertiesBuilder(OpenApiDocConverter(doc), config);
 const properties = parser.build();
+
 
 export class v20250926 implements INodeType {
 	description: INodeTypeDescription = {
---
+++ test.ts
@@ -0,0 +1,14 @@
+import doc from './nodes/Qonekto/v20250926/openapi.json';
+import OpenApiDocConverter from './nodes/Qonekto/openapi/OpenApiDocConverter';
+import { N8NPropertiesBuilder, N8NPropertiesBuilderConfig } from '@devlikeapro/n8n-openapi-node';
+import QonektoOperationParser from './nodes/Qonekto/openapi/QonektoOperationsParser';
+
+console.log(doc);
+
+const config: N8NPropertiesBuilderConfig = {
+	operation: new QonektoOperationParser(),
+};
+const parser = new N8NPropertiesBuilder(OpenApiDocConverter(doc), config);
+const properties = parser.build();
+
+console.log(properties);
