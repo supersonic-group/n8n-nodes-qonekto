4086dde0985ba26d43a0464a77643d73b9d01d33
Rouven Hurling <rouven@rhurling.de>
2025-10-12 11:01:42 +0200

    consolidate multiple 'Listen' field definitions into a single structure with optional pagination configuration

--- nodes/Qonekto/descriptions/Listen/Fields.ts
+++ nodes/Qonekto/descriptions/Listen/Fields.ts
@@ -1,229 +1,51 @@
 import { INodeProperties } from 'n8n-workflow';
 
-export const Anreden: INodeProperties[] = [
+export const Fields: INodeProperties[] = [
 	{
-		displayName: 'Per Page',
-		name: 'per_page',
-		description: 'Enables Pagination and returns results paginated by given per_page',
-		default: 100,
-		type: 'number',
-		routing: {
-			send: {
-				type: 'query',
-				property: 'per_page',
-				value: '={{ $value }}',
-				propertyInDotNotation: false,
+		displayName: 'Optional Pagination',
+		name: 'optional pagination',
+		type: 'collection',
+		placeholder: 'Add Field',
+		default: {},
+		options: [
+			{
+				displayName: 'Per Page',
+				name: 'per_page',
+				description: 'Enables Pagination and returns results paginated by given per_page',
+				default: 100,
+				type: 'number',
+				routing: {
+					send: {
+						type: 'query',
+						property: 'per_page',
+						value: '={{ $value }}',
+						propertyInDotNotation: false,
+					},
+				},
 			},
-		},
+			{
+				displayName: 'Page',
+				name: 'page',
+				description: 'Returns result of given page number, when Pagination is enabled',
+				default: 1,
+				hint: 'Requires "Per Page" to be set to enable Pagination',
+				type: 'number',
+				routing: {
+					send: {
+						type: 'query',
+						property: 'page',
+						value: '={{ $value }}',
+						propertyInDotNotation: false,
+					},
+				},
+			},
+		],
 		displayOptions: {
 			show: {
 				resource: ['Listen'],
-				operation: ['Anreden'],
 			},
 		},
 	},
 ];
 
-export const Gesellschaften: INodeProperties[] = [
-	{
-		displayName: 'Per Page',
-		name: 'per_page',
-		description: 'Enables Pagination and returns results paginated by given per_page',
-		default: 100,
-		type: 'number',
-		routing: {
-			send: {
-				type: 'query',
-				property: 'per_page',
-				value: '={{ $value }}',
-				propertyInDotNotation: false,
-			},
-		},
-		displayOptions: {
-			show: {
-				resource: ['Listen'],
-				operation: ['Gesellschaften'],
-			},
-		},
-	},
-];
-
-export const KundenDetailFelder: INodeProperties[] = [
-	{
-		displayName: 'Per Page',
-		name: 'per_page',
-		description: 'Enables Pagination and returns results paginated by given per_page',
-		default: 100,
-		type: 'number',
-		routing: {
-			send: {
-				type: 'query',
-				property: 'per_page',
-				value: '={{ $value }}',
-				propertyInDotNotation: false,
-			},
-		},
-		displayOptions: {
-			show: {
-				resource: ['Listen'],
-				operation: ['Kunden Detail Felder'],
-			},
-		},
-	},
-];
-
-export const Länder: INodeProperties[] = [
-	{
-		displayName: 'Per Page',
-		name: 'per_page',
-		description: 'Enables Pagination and returns results paginated by given per_page',
-		default: 100,
-		type: 'number',
-		routing: {
-			send: {
-				type: 'query',
-				property: 'per_page',
-				value: '={{ $value }}',
-				propertyInDotNotation: false,
-			},
-		},
-		displayOptions: {
-			show: {
-				resource: ['Listen'],
-				operation: ['Länder'],
-			},
-		},
-	},
-];
-
-export const Rechtsformen: INodeProperties[] = [
-	{
-		displayName: 'Per Page',
-		name: 'per_page',
-		description: 'Enables Pagination and returns results paginated by given per_page',
-		default: 100,
-		type: 'number',
-		routing: {
-			send: {
-				type: 'query',
-				property: 'per_page',
-				value: '={{ $value }}',
-				propertyInDotNotation: false,
-			},
-		},
-		displayOptions: {
-			show: {
-				resource: ['Listen'],
-				operation: ['Rechtsformen'],
-			},
-		},
-	},
-];
-
-export const Sparten: INodeProperties[] = [
-	{
-		displayName: 'Per Page',
-		name: 'per_page',
-		description: 'Enables Pagination and returns results paginated by given per_page',
-		default: 100,
-		type: 'number',
-		routing: {
-			send: {
-				type: 'query',
-				property: 'per_page',
-				value: '={{ $value }}',
-				propertyInDotNotation: false,
-			},
-		},
-		displayOptions: {
-			show: {
-				resource: ['Listen'],
-				operation: ['Sparten'],
-			},
-		},
-	},
-];
-
-export const Status: INodeProperties[] = [
-	{
-		displayName: 'Per Page',
-		name: 'per_page',
-		description: 'Enables Pagination and returns results paginated by given per_page',
-		default: 100,
-		type: 'number',
-		routing: {
-			send: {
-				type: 'query',
-				property: 'per_page',
-				value: '={{ $value }}',
-				propertyInDotNotation: false,
-			},
-		},
-		displayOptions: {
-			show: {
-				resource: ['Listen'],
-				operation: ['Status'],
-			},
-		},
-	},
-];
-
-export const Vermittler: INodeProperties[] = [
-	{
-		displayName: 'Per Page',
-		name: 'per_page',
-		description: 'Enables Pagination and returns results paginated by given per_page',
-		default: 100,
-		type: 'number',
-		routing: {
-			send: {
-				type: 'query',
-				property: 'per_page',
-				value: '={{ $value }}',
-				propertyInDotNotation: false,
-			},
-		},
-		displayOptions: {
-			show: {
-				resource: ['Listen'],
-				operation: ['Vermittler'],
-			},
-		},
-	},
-];
-
-export const Zahlweisen: INodeProperties[] = [
-	{
-		displayName: 'Per Page',
-		name: 'per_page',
-		description: 'Enables Pagination and returns results paginated by given per_page',
-		default: 100,
-		type: 'number',
-		routing: {
-			send: {
-				type: 'query',
-				property: 'per_page',
-				value: '={{ $value }}',
-				propertyInDotNotation: false,
-			},
-		},
-		displayOptions: {
-			show: {
-				resource: ['Listen'],
-				operation: ['Zahlweisen'],
-			},
-		},
-	},
-];
-
-export default [
-	...Anreden,
-	...Gesellschaften,
-	...KundenDetailFelder,
-	...Länder,
-	...Rechtsformen,
-	...Sparten,
-	...Status,
-	...Vermittler,
-	...Zahlweisen,
-];
+export default Fields;
