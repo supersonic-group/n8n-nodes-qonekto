022b1b76ca6fcb740ac66058d3df8f32e0efe142
Rouven Hurling <rouven@supersonic-group.com>
2026-03-27 09:54:57 +0100

    refactor(Qonekto): register new resources and reorder to match OpenAPI tags
    - Add all 8 new resources to Resources.ts, Operations.ts, Fields.ts
    - Reorder resources to match OpenAPI tag order
    - Remove duplicate Kunden-Tag directory (replaced by KundeTags)

--- nodes/Qonekto/descriptions/Fields.ts
+++ nodes/Qonekto/descriptions/Fields.ts
@@ -1,9 +1,31 @@
 import Kunde from './Kunde/Fields';
-import KundeTag from './Kunden-Tag/Fields';
+import KundeTags from './KundeTags/Fields';
+import KundeAdditionalAddresses from './KundeAdditionalAddresses/Fields';
+import KundeNotes from './KundeNotes/Fields';
+import KundeRelations from './KundeRelations/Fields';
+import KundeNotifications from './KundeNotifications/Fields';
 import Vertrag from './Vertrag/Fields';
+import ContractBankAccount from './ContractBankAccount/Fields';
 import Listen from './Listen/Fields';
 import Panda from './Panda/Fields';
+import ClaimsSchaden from './ClaimsSchaden/Fields';
+import TasksAufgaben from './TasksAufgaben/Fields';
 import Pipeline from './Pipeline/Fields';
 import Misc from './Misc/Fields';
 
-export default [...Kunde, ...KundeTag, ...Vertrag, ...Listen, ...Panda, ...Pipeline, ...Misc];
+export default [
+	...Kunde,
+	...KundeTags,
+	...KundeAdditionalAddresses,
+	...KundeNotes,
+	...KundeRelations,
+	...KundeNotifications,
+	...Vertrag,
+	...ContractBankAccount,
+	...Listen,
+	...Panda,
+	...ClaimsSchaden,
+	...TasksAufgaben,
+	...Pipeline,
+	...Misc,
+];
